a90c4d75024f61d6e72b5540acfe43071d3dc8aa
Lordraven19 <62838829+Lordraven19@users.noreply.github.com>
2023-06-10 19:38:03 +0100

    Stretch no longer available - using debian testing instead

--- install-sysbox.sh
+++ install-sysbox.sh
@@ -12,13 +12,13 @@ sleep 2.5
 sudo apt-get install qemu-user-static schroot debootstrap
 echo "Setting up..."
 sleep 2.5
-debootstrap --arch i386 --foreign stretch /srv/chroot/debian-x86 http://deb.debian.org/debian
+debootstrap --arch i386 --foreign testing /srv/chroot/debian-x86 http://deb.debian.org/debian
 cp /usr/bin/qemu-i386-static /srv/chroot/debian-x86/usr/bin
 chroot "/srv/chroot/debian-x86" /debootstrap/debootstrap --second-stage
 echo "Creating the config file..."
 sleep 2.5
 sudo echo '[debian-x86]
-description=Debian Stretch x86 chroot
+description=Debian Testing x86 chroot
 aliases=debian-x86
 type=directory
 directory=/srv/chroot/debian-x86
